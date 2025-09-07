8a0a3e2e81b46d81c4e5c9fc0a684d1b4034a640
felixcao99 <felixcao99@hotmail.com>
2025-09-06 21:14:29 -0700

    finished file storage

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -1,8 +1,14 @@
 package main
 
 import (
+	// "encoding/base64"
 	"fmt"
+	"io"
+	"mime"
 	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
 	"github.com/google/uuid"
@@ -28,10 +34,88 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
 	// TODO: implement the upload here
+	const maxMemory = 10 << 20
+	r.ParseMultipartForm(maxMemory)
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	file, header, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to parse form file", err)
+		return
+	}
+	defer file.Close()
+
+	contenttype := header.Header.Get("Content-Type")
+	mediaType, _, err := mime.ParseMediaType(contenttype)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Can't parse media type", err)
+	}
+	if mediaType != "image/jpeg" && mediaType != "image/png" {
+		respondWithError(w, http.StatusBadRequest, "Thumbnail must be a JPEG or PNG image", nil)
+		return
+	}
+	ext := strings.Split(mediaType, "/")[1]
+	wd, _ := os.Getwd()
+	filepath := filepath.Join(wd, cfg.assetsRoot, fmt.Sprintf("%s.%s", videoID.String(), ext))
+
+	fmt.Println("creating file at", filepath)
+
+	thumbfile, err := os.Create(filepath)
+
+	if err != nil {
+		fmt.Println("Error happened when creating file", err)
+		respondWithError(w, http.StatusInternalServerError, "Unable to create file", err)
+		return
+	}
+	defer thumbfile.Close()
+
+	fmt.Println("copying file...")
+	_, err = io.Copy(thumbfile, file)
+	if err != nil {
+		fmt.Println("Error happened when copying file", err)
+		respondWithError(w, http.StatusBadRequest, "Unable to create file", err)
+		return
+	}
+
+	thumbfileurl := fmt.Sprintf("http://localhost:%s/assets/%s.%s", cfg.port, videoID.String(), ext)
+
+	// imagedata, err := io.ReadAll(file)
+	// if err != nil {
+	// 	respondWithError(w, http.StatusInternalServerError, "Unable to read file", err)
+	// 	return
+	// }
+
+	// imagebase64 := base64.StdEncoding.EncodeToString(imagedata)
+	// imagebase64url := fmt.Sprintf("data:%s;base64,%s", mediaType, imagebase64)
+
+	video, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Unable to get video", err)
+		return
+	}
+	if video.UserID != userID {
+		respondWithError(w, http.StatusUnauthorized, "You don't have permission to upload a thumbnail for this video", nil)
+		return
+	}
+
+	// thumbnail := thumbnail{
+	// 	data:      imagedata,
+	// 	mediaType: mediaType,
+	// }
+
+	// videoThumbnails[videoID] = thumbnail
+	// tburl := fmt.Sprintf("http://localhost:8091/api/thumbnails/%s", videoID.String())
+	// video.ThumbnailURL = &tburl
+	// video.ThumbnailURL = &imagebase64url
+	video.ThumbnailURL = &thumbfileurl
+
+	err = cfg.db.UpdateVideo(video)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Unable to update video", err)
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, video)
 }
